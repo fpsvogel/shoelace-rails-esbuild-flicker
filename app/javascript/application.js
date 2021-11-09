32b703b1f8d21d02e8e0e1ff5723a3f367eb2c96
Felipe Vogel <fps.vogel@gmail.com>
2021-11-09 09:04:41 -0500

    attempt to tear down tab group via Stimulus controller#disconnect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,25 @@ import "./controllers"
 
 import { setBasePath } from '@shoelace-style/shoelace'
 // icons don't appear if set to "../../public/shoelace/dist/"
-setBasePath("../../public/shoelace/dist/")
+// setBasePath("../../public/shoelace/dist/")
 // icons do appear if set to CDN.
-// setBasePath("https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.57/dist/")
+setBasePath("https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.57/dist/")
+
+
+// // app/javascript/packs/application.js
+// document.addEventListener('turbolinks:before-cache', () => {
+//   application.controllers.forEach(controller => {
+//     if (typeof controller.teardown === 'function') {
+//       controller.teardown()
+//     }
+//   })
+// })
+
+
+// const tagsUsed = ['sl-icon', 'sl-alert', 'sl-tab-panel', 'sl-tab-group', 'sl-tab'];
+
+// Promise.all(tagsUsed.map(tag => customElements.whenDefined(tag))).then(() => {
+//   // all components have loaded now, transition in and show the UI here.
+//   document.querySelector("sl-tab-group").style.visibility = "visible"
+//   console.log("Done loading.");
+// });
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    document.querySelector("sl-tab-group").style.visibility = "visible"
+    console.log("connected")
+  }
+
+  disconnect() {
+    document.querySelector("sl-tab-group").style.visibility = "hidden"
+    console.log("disconnected")
   }
 }
